7d068ec748cba0b09c8f9db822c938499a2f146e
generalov <alexander.generalov1987@gmail.com>
2024-08-17 18:42:48 +0300

    settings.py corrected

--- 2.1/orm_shop/orm_shop/settings.py
+++ 2.1/orm_shop/orm_shop/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'dacent0000',
+        'PASSWORD': 'secret',
         'HOST': 'localhost',
         'PORT': 5432
     }
