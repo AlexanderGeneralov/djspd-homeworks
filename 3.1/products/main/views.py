6216fd961d7d59e096372b4ab6435841860f88fa
generalov <alexander.generalov1987@gmail.com>
2024-08-24 14:59:46 +0300

    homework DRF

--- 3.1/products/main/views.py
+++ 3.1/products/main/views.py
@@ -1,8 +1,11 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from main.models import Product, Review
 from main.serializers import ReviewSerializer, ProductListSerializer, ProductDetailsSerializer
 
 
@@ -11,15 +14,38 @@ def products_list_view(request):
     """реализуйте получение всех товаров из БД
     реализуйте сериализацию полученных данных
     отдайте отсериализованные данные в Response"""
-    pass
+    products = Product.objects.all()
+    ser = ProductListSerializer(products, many=True)
+    return Response(ser.data)
+
+
+@api_view(['GET'])
+def review_list_view(request):
+    reviews = Review.objects.all()
+    ser = ReviewSerializer(reviews, many=True)
+    return Response(ser.data)
+
+
+@api_view(['GET'])
+def product_details_view(requeset):
+    product = Product.objects.get(id=1)
+    ser = ProductDetailsSerializer(product)
+    return Response(ser.data)
 
 
 class ProductDetailsView(APIView):
+
     def get(self, request, product_id):
         """реализуйте получение товара по id, если его нет, то выдайте 404
         реализуйте сериализацию полученных данных
         отдайте отсериализованные данные в Response"""
-        pass
+        try:
+            product = Product.objects.get(pk=product_id)
+
+            ser = ProductDetailsSerializer(product, many=False)
+            return Response(ser.data)
+        except Product.DoesNotExist:
+            raise Http404('Product not found')
 
 
 # доп задание:
@@ -29,4 +55,10 @@ class ProductFilteredReviews(APIView):
         реализуйте получение отзывов по конкретному товару с определённой оценкой
         реализуйте сериализацию полученных данных
         отдайте отсериализованные данные в Response"""
-        pass
+
+        mark_val = request.query_params.get('mark')
+
+        product = Product.objects.filter(pk=product_id, comments__mark=mark_val)
+        ser = ProductDetailsSerializer(product, many=False)
+        return Response(ser.data)
+
